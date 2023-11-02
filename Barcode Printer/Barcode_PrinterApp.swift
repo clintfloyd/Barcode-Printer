7daa9cc72268a1a7d655e722be3e9874f2bda890
Clint Monzon <clint@ativistar.com>
2023-11-02 12:37:41 +0400

    Initial Commit

---
+++ Barcode Printer/Barcode_PrinterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Barcode_PrinterApp.swift
+//  Barcode Printer
+//
+//  Created by Clint Monzon on 02/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Barcode_PrinterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
